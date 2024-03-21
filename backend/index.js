83b566f628ad65534342efd351e7862d876b74c5
Farzeel <farzeelaftab544@gmail.com>
2024-03-21 01:10:39 +0100

    deploying ready

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(cors({
 }))
 app.use(express.json({limit:"16kb"}));
 app.use(express.static("public"))
+job.start()
 
 
 connectDb().then(()=>{
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 // ROUTES IMPORT
 import feedbackRoute from "./src/routes/feedback.routes.js";
+import { job } from "./cron.js";
 
 
 
